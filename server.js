6e7ed542134ca493c5591616741d48b362608241
Haralambos <lampistsivikas@yahoo.com>
2020-03-09 17:59:51 -0400

    new message

---
+++ server.js
@@ -0,0 +1,14 @@
+// Here we add all the necessary code in order for our application to work //
+var express = require("express");
+
+var app = express(),
+    path = require("path");
+
+app.set("port", process.env.PORT || 8000);
+
+// Telling the app to use the public folder to serve our files is a nice and neat way to structure our code//
+
+app.use(express.static("public"));
+
+app.listen(8000);
+console.log("App is listening on Port" + Port);
